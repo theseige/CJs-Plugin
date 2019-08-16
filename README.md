ce62d024dc8756a6e51b92c754e37899d7511234
Carl Andren <cjandren@gmail.com>
2019-08-16 18:59:58 -0400

    Added Features
    The plugin now produces a settings page in the Admin section of WordPress named CJs Options
    Still no real functionality built into the plugin

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
  My test plugin where I develop and learn.
  
  The current plugin simply says "Thanks for enabling my plugin!" upon activation.
- 
- Testing how branches and repos work.
--- cjs-plugin.php
+++ cjs-plugin.php
@@ -15,7 +15,8 @@ License: GPL2
 Begin Activation Functions
 */
 
-function generate_activation_notice() {
+function generate_activation_notice()
+{
     ?>
     <div class="notice notice-success is-dismissable">
         <p><?php _e( 'Thanks for enabling my plugin!', 'cjs-text-domain'); ?></p>
@@ -23,13 +24,15 @@ function generate_activation_notice() {
     <?php
 }
 
-function cjs_plugin_activation() {
+function cjs_plugin_activation()
+{
     add_option('Activated_Plugin', 'CJs-Plugin');
 }
 
 register_activation_hook(__FILE__, 'cjs_plugin_activation' );
 
-function load_plugin() {
+function load_plugin()
+{
     if ( is_admin && get_option('Activated_Plugin' ) == 'CJs-Plugin' ) {
         delete_option('Activated_Plugin');
 	    add_action( 'admin_notices', 'generate_activation_notice');
@@ -42,6 +45,65 @@ add_action(admin_init, load_plugin());
 /*
  * Below here will be the core functionality of our plugin
  */
+
+function cjs_add_class_to_body()
+{
+    if ( !is_admin() ) {
+        $classes[] = 'cj-has-a-class';
+    }
+    return $classes;
+}
+
+function cjs_options_page()
+{
+    add_menu_page(
+            'CJs',
+        'CJs Options',
+        'manage_options',
+        'cjs',
+        'cjs_options_page_html',
+        '',
+        20
+    );
+}
+
+/*
+ * This is the default html for the options page in the Admin Menu
+ */
+function cjs_options_page_html()
+{
+    if ( ! current_user_can( 'manage_options' ) ) {
+        return;
+    }
+    ?>
+        <div class="wrap">
+            <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+            <form action=""options.php" method="post">
+                <?php
+                settings_fields( 'cjs_options');
+                do_settings_sections( 'cjs' );
+                submit_button( 'Save Settings' );
+                ?>
+            </form>
+            <p><?php echo esc_html('It works!' ); ?></p>
+        </div>
+    <?php
+}
+
+/*
+ * Enabling this action will remove the default Options page in the Admin Menu
+ * It is still directly accessible.
+ * Use add_action('admin_menu', 'cjs_options_page_remove');
+ * Must be below add_action('admin_menu', 'cjs_options_page');
+ */
+
+function cjs_options_page_remove()
+{
+    remove_menu_page('cjs');
+}
+
+add_action('admin_menu', 'cjs_options_page');
+add_filter('body_class', 'cjs_add_class_to_body');
 
 
 /*
